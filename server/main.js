49d56f13411b7549f11edb21855e6a7cb1a3d479
Corneezius <cfr2116@columbia.edu>
2018-06-18 10:27:35 -0400

    add fake records to database

--- server/main.js
+++ server/main.js
@@ -3,16 +3,16 @@ import { Meteor } from "meteor/meteor";
 import { Employees } from "../imports/collections/employees";
 import {image, helpers} from "faker";
 
-Meteor.startup() => {
+Meteor.startup(() => {
   const numberRecords = Employees.find({}).count();
-  console.log(numberRecords)
     if (!numberRecords) {
-    _.times(5000, () => {
+      _.times(5000, () => {
       const { name, email, phone} = helpers.createCard();
+
       Employees.insert({
         name,email, phone,
         avatar: image.avatar()
       });
     });
   };
-};
+});

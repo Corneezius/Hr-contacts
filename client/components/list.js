c6bcd3e38b0b9da64f8190cbe8855e395f3f4e7d
Corneezius <cfr2116@columbia.edu>
2018-06-18 12:14:41 -0400

    update click so more employees rendered

--- client/components/list.js
+++ client/components/list.js
@@ -3,6 +3,8 @@ import { withTracker } from "meteor/react-meteor-data";
 import { Employees} from "../../imports/collections/employees";
 import Detail from "./detail";
 
+const PER_PAGE = 20;
+
 const List = (props) => {
 
   return (
@@ -10,7 +12,7 @@ const List = (props) => {
       <div className="employee-list">
         {props.employees.map(employee => <Detail key={employee._id} employee={employee} />)}
       </div>
-      <button onClick={() => console.log("clicked")} className="btn btn-primary">Load More...</button>
+      <button onClick={() => Meteor.subscribe("employees", 40)} className="btn btn-primary">Load More...</button>
     </div>
   );
 };
@@ -18,7 +20,7 @@ const List = (props) => {
 
 export default withTracker(() => {
  // Setup subscription
- Meteor.subscribe('employees');
+ Meteor.subscribe('employees', PER_PAGE);
  return { employees: Employees.find({}).fetch() };
  // return an object, return is sent to EmployeeList as props
 }) (List);

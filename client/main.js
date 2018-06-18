1c4a891a7db8a4a0fc82d56d9a1f5705789a3436
Corneezius <cfr2116@columbia.edu>
2018-06-18 09:51:29 -0400

    get something on screen

--- client/main.js
+++ client/main.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>Hello</div>
+  );
+};
+
 
 Meteor.startup(() => {
-// React render
+  ReactDOM.render(<App />, document.querySelector(".container"));
 });

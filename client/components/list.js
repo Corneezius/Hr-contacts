6e5620d05d8429d73518e945361ba39ddeed4da4
Corneezius <cfr2116@columbia.edu>
2018-06-18 10:44:47 -0400

    create employee list component

---
+++ client/components/list.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div>
+      <div className="employee-list">
+        Employee List
+      </div>
+    </div>
+  );
+};
+
+export default List;
--- client/main.js
+++ client/main.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import List from "./components/list";
+
 const App = () => {
   return (
-    <div>Hello</div>
+    <div>
+      <List />
+    </div>
   );
 };
 
